b32166839066813853aa4c227791324b71ba981f
Ian Melendez <imelendez@preludedynamics.com>
2021-06-02 10:18:05 -0500

    context WIP

--- src/Day15/Soln.hs
+++ src/Day15/Soln.hs
@@ -1,13 +1,29 @@
 module Day15.Soln where 
 
+import qualified Data.Map as Map
+
 inputFile :: FilePath 
 inputFile = "src/Day15/short-input.txt"
 
-readInput :: IO [String]
-readInput = readCSV <$> readFile inputFile
+readInput :: IO [Int]
+readInput = map read . readCSV <$> readFile inputFile
 
 readCSV :: String -> [String]
 readCSV [] = []
 readCSV input = 
   let (item, rest) = span (/= ',') input
    in item : readCSV (dropWhile (== ',') rest)
+
+--------------------------------------------------------------------------------
+-- Rules
+
+{-
+0 -> last one was a new number 
+N -> period between last number and previous instance
+-}
+
+type NumToOccurence = Map.Map Int Int
+data NumContext = NC Int NumToOccurence -- (current index, num to last index)
+
+initNumContext :: [Int] -> NumContext
+initNumContext ns = undefined
