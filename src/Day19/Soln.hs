7d8fd1abde411d1f15bacd40f97f2380bcbaab78
Ian Melendez <ian.melendez.95@utexas.edu>
2021-11-09 22:43:37 -0600

    19.1 - complex rule resolution

--- src/Day19/Soln.hs
+++ src/Day19/Soln.hs
@@ -5,10 +5,37 @@ import qualified Data.Text.IO as TIO
 
 import qualified Data.IntMap.Strict as IntMap
 
+import Data.List
+
 import qualified Day19.Parse as P
 
 
 type Rules = IntMap.IntMap P.Rule
+
+test :: IO ()
+test = 
+  do (decls, msgs) <- readSpec
+     let rules = collectRules decls
+     print $ resolveCRule rules 0
+
+
+-- Complex Rule
+data CRule = CRExact  !Char
+           | CRAltSeq ![[CRule]]
+
+instance Show CRule where 
+  show (CRExact c) = "\"" ++ [c] ++ "\""
+  show (CRAltSeq seqs) = 
+    "( " ++ intercalate " | " (map (unwords . map show) seqs) ++ " )"
+
+resolveCRule :: Rules -> Int -> CRule
+resolveCRule rules idx = 
+  case IntMap.lookup idx rules of 
+    Nothing -> error $ "Rule index doesn't exist: " ++ show idx
+    Just (P.RExact c) -> CRExact c
+    Just (P.RAltSeq rule_seqs) -> 
+      CRAltSeq $ map (map (resolveCRule rules)) rule_seqs
+
 
 collectRules :: [P.Decl] -> Rules
 collectRules = IntMap.fromList
